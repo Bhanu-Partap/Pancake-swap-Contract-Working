2bc33931b0a45a79a161bd09d1b085fe6b61a32b
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-23 15:30:03 +0530

    functions added

--- Abiinfo.js
+++ Abiinfo.js
@@ -1,3 +1,4 @@
 const erc20 = ["function decimals() public view returns (uint8)"]
 // Return the address of Liquidity Provider Contract 
 const factoryAbi =["function getPair(address tokenA, address tokenB) external view returns (address pair)"]
+const pairABI=["function token0() external view returns (address)","function token1() external view returns (address)", "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"]
