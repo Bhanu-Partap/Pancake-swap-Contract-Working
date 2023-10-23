e7ecb98f35b393b8bd01046b378455e6076dbc45
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-23 14:45:18 +0530

    addresses noted

--- AddressList.js
+++ AddressList.js
@@ -1,7 +1,7 @@
-const FactoryAddress= "";
-const RouterAddress= "";
-const FromAddress= "";      //BUSD
-const ToAddress= "";        // BNB
+const FactoryAddress= "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+const RouterAddress= "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const FromAddress= "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";      //BUSD
+const ToAddress= "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c ";       // BNB
 
 module.exports={
     FactoryAddress,
