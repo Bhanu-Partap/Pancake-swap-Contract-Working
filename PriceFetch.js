423cc425335c3011e32639bca1ac9d8e4b039fde
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-24 10:28:46 +0530

    functions running

--- PriceFetch.js
+++ PriceFetch.js
@@ -4,7 +4,7 @@ const {
     FactoryAddress,
     RouterAddress,
     FromAddress,
-    toAddress
+    ToAddress
 }= require("./AddressList")
 const {
     erc20, factoryAbi, pairABI, routerAbi
@@ -13,10 +13,10 @@ const {
 const Provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.binance.org/")
 const factoryInstance = new ethers.Contract(FactoryAddress, factoryAbi, Provider )
 const routerInstance = new ethers.Contract(RouterAddress, routerAbi, Provider )
-console.log(routerInstance);
+// console.log(routerInstance);
 const priceFetch = async(amount)=>{
     const token1 = new ethers.Contract(FromAddress, erc20 , Provider )
-    const token2 = new ethers.Contract(toAddress, erc20 , Provider )
+    const token2 = new ethers.Contract(ToAddress, erc20 , Provider )
 
     const decimal1 = await token1.decimals()
     console.log(decimal1);
