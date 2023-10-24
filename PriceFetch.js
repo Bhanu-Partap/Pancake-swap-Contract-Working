db72b4574919c6fc179eebf2ae7c37ea1e03e0f0
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-24 12:31:29 +0530

    error resolved and price fetch completed

--- PriceFetch.js
+++ PriceFetch.js
@@ -8,7 +8,8 @@ const {
 } = require("./AddressList");
 const { erc20, factoryAbi, pairABI, routerAbi } = require("./Abiinfo");
 
-const Provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.defibit.io/");
+const Provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.binance.org/");
+// console.log(Provider);
 const factoryInstance = new ethers.Contract(
   FactoryAddress,
   factoryAbi,
@@ -19,10 +20,11 @@ const routerInstance = new ethers.Contract(RouterAddress, routerAbi, Provider);
 const priceFetch = async (humanFormat) => {
   const token1 = new ethers.Contract(FromAddress, erc20, Provider);
   const token2 = new ethers.Contract(ToAddress, erc20, Provider);
-
   const decimal1 = await token1.decimals();
   const decimal2 = await token2.decimals();
+  console.log(decimal2);
   const amountIn = ethers.utils.parseUnits(humanFormat,decimal1).toString();
+
   const amountsOut = await routerInstance.getAmountsOut(amountIn, [
     FromAddress,
     ToAddress,
