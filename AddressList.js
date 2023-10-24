ef43005a66589506c82a9f24612e668210447bee
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-24 14:05:41 +0530

    New token addresses added

--- AddressList.js
+++ AddressList.js
@@ -7,9 +7,9 @@
 
 //=====================Uniswap
 const FactoryAddress= "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-const RouterAddress= "0x10ED43C718714eb63d5aA57B78B54704E256024E";
-const FromAddress= "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";      //BUSD
-const ToAddress= "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";       // BNB
+const RouterAddress= "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const FromAddress= "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";      //WETH
+const ToAddress= "0x6B175474E89094C44Da98b954EedeAC495271d0F";       // DAI
 
 module.exports={
     FactoryAddress,
