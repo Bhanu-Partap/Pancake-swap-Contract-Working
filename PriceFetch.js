d57ce88932f288316865955fbd20eaa367fabe3c
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-24 10:40:49 +0530

    function updated  price fetch

--- PriceFetch.js
+++ PriceFetch.js
@@ -1,25 +1,33 @@
-const {ethers, JsonRpcProvider} = require("ethers")
+const { ethers, JsonRpcProvider } = require("ethers");
 
 const {
-    FactoryAddress,
-    RouterAddress,
-    FromAddress,
-    ToAddress
-}= require("./AddressList")
-const {
-    erc20, factoryAbi, pairABI, routerAbi
-}= require("./Abiinfo")
+  FactoryAddress,
+  RouterAddress,
+  FromAddress,
+  ToAddress,
+} = require("./AddressList");
+const { erc20, factoryAbi, pairABI, routerAbi } = require("./Abiinfo");
 
-const Provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.binance.org/")
-const factoryInstance = new ethers.Contract(FactoryAddress, factoryAbi, Provider )
-const routerInstance = new ethers.Contract(RouterAddress, routerAbi, Provider )
+const Provider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed1.binance.org/"
+);
+const factoryInstance = new ethers.Contract(
+  FactoryAddress,
+  factoryAbi,
+  Provider
+);
+const routerInstance = new ethers.Contract(RouterAddress, routerAbi, Provider);
 // console.log(routerInstance);
-const priceFetch = async(amount)=>{
-    const token1 = new ethers.Contract(FromAddress, erc20 , Provider )
-    const token2 = new ethers.Contract(ToAddress, erc20 , Provider )
+const priceFetch = async (humanFormat) => {
+  const token1 = new ethers.Contract(FromAddress, erc20, Provider);
+  const token2 = new ethers.Contract(ToAddress, erc20, Provider);
 
-    const decimal1 = await token1.decimals()
-    console.log(decimal1);
-}
+  const decimal1 = await token1.decimals();
+  const amountIn = ethers.utils.parseUnits(humanFormat.decimals).toString();
+  const getAmountsOut = await routerInstance.getAmountsOut(amountIn, [
+    FromAddress,
+    ToAddress,
+  ]);
+};
 
-priceFetch()
+priceFetch();
