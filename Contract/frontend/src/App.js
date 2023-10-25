9b16aa7e40b9b99a5af128b62c3ce62b5a9ec3fd
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-25 10:40:00 +0530

    navbar setup

--- Contract/frontend/src/App.js
+++ Contract/frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/js/bootstrap.min';
 import Navbar from './Components/Navbar';
 
 function App() {
--- Contract/frontend/src/Components/Navbar.js
+++ Contract/frontend/src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" to="#">
-            Navbar
+            Price Fetch DAAP
           </a>
           <button
             className="navbar-toggler"
@@ -22,8 +22,8 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" to="#">
-                  Home
+                <a className="nav-link active center" aria-current="page" to="#">
+                  Swap
                 </a>
               </li>
               {/* <li className="nav-item">
@@ -49,17 +49,6 @@ export default function Navbar() {
                 </a>
               </li> */}
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
