376a04db5db50a48ba7c5b94237dc4df90f8523a
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-25 12:46:44 +0530

    home page

--- Contract/frontend/src/Components/Homepage.js
+++ Contract/frontend/src/Components/Homepage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Col, Row} from "react-bootstrap"
+import { FcSettings } from 'react-icons/fc';
 
 export default function Homepage() {
   return (
     <>
-      <h1 id='home'>HOMEPAGE</h1>
+      <section id="home">
+        <Row>
+          <Col>
+            <Row>
+              <h3>Swap</h3>
+              <h3>Buy</h3>
+              <p><FcSettings/></p>
+            </Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+          </Col>
+        </Row>
+      </section>
     </>
     )
 }
